6b1c590a079b30d7868f174d26bafe7fe17afbd1
Jeffrey Ramo <matheusunitt@gmail.com>
2023-04-01 09:20:56 -0300

    Adiciona ícone e botão do github, altera texto e estilização dos botões no Hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,13 @@
 import "./styles.css";
 
 import linkedin from "../../assets/images/linkedin.png";
+import github from "../../assets/images/github.png";
 
 import Button from "../Button";
 
 const Hero = ({ children }) => {
   const linkedin_url = "https://www.linkedin.com/in/jeffreyalvr/";
+  const github_url = "https://github.com/jeffreyalvr";
   return (
     <div className="hero">
       {children}
@@ -16,11 +18,10 @@ const Hero = ({ children }) => {
         <div className="label"> react js developer</div>
         <div className="carrot" />
       </h2>
-      <Button
-        text="Encontre-me no LinkedIn"
-        icon={linkedin}
-        url={linkedin_url}
-      />
+      <div className="row">
+        <Button text="LinkedIn" icon={linkedin} url={linkedin_url} />
+        <Button text="GitHub" icon={github} url={github_url} />
+      </div>
       <span>Este site ainda se encontra em desenvolvimento.</span>
     </div>
   );
