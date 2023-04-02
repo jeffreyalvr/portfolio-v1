4e8d80464f7eed497769fa8da2023d23faf2b17d
Jeffrey Ramo <matheusunitt@gmail.com>
2023-04-02 15:48:04 -0300

    Adiciona responsividade no site, encapsula o conteúdo interno do hero em um div container e suas devidas estilizações

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -19,30 +19,32 @@ const Hero = ({ children }) => {
   return (
     <div className="hero">
       {children}
-      <h1>
-        {lang === "pt-br"
-          ? book.pt_br.hero.index.hero_h1
-          : book.en_ca.hero.index.hero_h1}
-        <span>{"{ Jeffrey }"}</span>
-      </h1>
-      <h2>
-        <div className="label"> react js developer</div>
-        <div className="carrot" />
-      </h2>
-      <div className="row">
-        <Button text="LinkedIn" icon={linkedin} url={linkedin_url} />
-        <Button text="GitHub" icon={github} url={github_url} />
-      </div>
-      <footer>
-        <div className="col">
-          <span>
-            {lang === "pt-br"
-              ? book.pt_br.hero.index.hero_footer_span
-              : book.en_ca.hero.index.hero_footer_span}
-          </span>
-          <span>&copy; 2023 - Jeffrey.</span>
+      <div className="container">
+        <h1>
+          {lang === "pt-br"
+            ? book.pt_br.hero.index.hero_h1
+            : book.en_ca.hero.index.hero_h1}
+          <span>{"{ Jeffrey }"}</span>
+        </h1>
+        <h2>
+          <div className="label"> react js developer</div>
+          <div className="carrot" />
+        </h2>
+        <div className="row">
+          <Button text="LinkedIn" icon={linkedin} url={linkedin_url} />
+          <Button text="GitHub" icon={github} url={github_url} />
         </div>
-      </footer>
+        <footer>
+          <div className="col">
+            <span>
+              {lang === "pt-br"
+                ? book.pt_br.hero.index.hero_footer_span
+                : book.en_ca.hero.index.hero_footer_span}
+            </span>
+            <span>&copy; 2023 - Jeffrey.</span>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 };
